65fa801ab61fc2668ec7a817fc37e2abd1d3bb9b
kevin <kevin.vandenbreemen@gmail.com>
2022-07-20 18:23:04 -0700

    Get working demo going

--- src/main/kotlin/com/vandenbreemen/jgdv/compose/Composables.kt
+++ src/main/kotlin/com/vandenbreemen/jgdv/compose/Composables.kt
@@ -2,14 +2,25 @@ package com.vandenbreemen.jgdv.compose
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.window.Window
 import com.vandenbreemen.jgdv.dsl.Image
 import com.vandenbreemen.jgdv.dsl.Rect
+import kotlinx.coroutines.flow.StateFlow
+import androidx.compose.ui.window.application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 /**
  * Convert the given AWT color to a compose color
@@ -36,6 +47,51 @@ fun ImageView(image: Image) {
     }
 
 }
+
+@Composable
+fun AnimatedImageDisplay(imageToDisplay: StateFlow<Image>) {
+    Column(Modifier.fillMaxSize()) {
+        imageToDisplay.collectAsState().value.let { image->
+            ImageView(image)
+        }
+    }
+}
+
+
+/**
+ * Renders the given image each time it is updated
+ */
+fun main() = application{
+
+    val flow = MutableStateFlow(makeImage(0))
+
+
+    CoroutineScope(Dispatchers.Default).launch {
+
+        for(i in 0..500 step 10) {
+            flow.value = makeImage(i)
+            delay(100)
+        }
+
+    }
+
+    Window(onCloseRequest = ::exitApplication, title = "Displaying animated grid of squares demo") {
+
+        AnimatedImageDisplay(flow)
+
+    }
+}
+
+private fun makeImage(idx: Int): Image {
+    return Image().apply {
+        addShape(Rect(java.awt.Color.gray, idx.toFloat(), 10f, 10f, 10f))
+        val random = Random(System.nanoTime())
+        for(i in 1 .. 100) {
+            addShape(Rect(java.awt.Color.red, i*10f, random.nextFloat()*100, 10f, 10f))
+        }
+    }
+}
+
 
 @Composable
 @Preview
